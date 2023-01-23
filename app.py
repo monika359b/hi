5e4fc60c11bcd37889233e896441a3d0495eea75
monika359b <89681654+monika359b@users.noreply.github.com>
2023-01-23 20:36:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ async def check_api():
         return jsonify({"status": "The api_id/api_hash combination is correct"})
 
 if __name__ == '__main__':
-   app.run(debug = True)
+   app.run(host='0.0.0.0', port=5000)
