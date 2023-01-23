0bf674d647ed5589a812a82233cc3ed8347560dd
monika359b <89681654+monika359b@users.noreply.github.com>
2023-01-23 21:30:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,15 +14,15 @@ def generate_random_string(string_length=8):
     return ''.join(random.choice(letters) for i in range(string_length))
 
 @app.route('/check_api')
-async def check_api():
+def check_api():
     try:
         api_id = request.args.get('api_id')
         api_hash = request.args.get('api_hash')
         bot_token = '5711346303:AAGxyS-o-Aijdfqdhyz5EnwwBcAWkjO74AA'
-        asyncio.set_event_loop(asyncio.new_event_loop())
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
         client = TelegramClient(generate_random_string(8), api_id, api_hash)
-        loop = asyncio.get_event_loop()
-        await client.start(bot_token=bot_token)
+        loop.run_until_complete(client.start(bot_token=bot_token))
         client.disconnect()
         loop.close()
         return jsonify({"status": "The api_id/api_hash combination is correct"})
