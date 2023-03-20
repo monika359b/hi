e90cb3a2396445172fc8ff620fb261b7d1a30038
kumawatrohit537 <82073058+kumawatrohit537@users.noreply.github.com>
2023-03-21 00:46:12 +0530

    Add files via upload

---
+++ details.js
@@ -0,0 +1,15 @@
+module.exports={
+    token:"6157208507:AGIxpaDs9-cBkYoBQAGq_NfuVpYWMb-I0c",
+    mongo_url:"mongodb+srv:/..net/?retryWrites=true&w=majority",
+    admins:[5908965726],
+    curr:"USDT",
+    lvl:[40,20,10],
+    profit_percent:[4,4.5,5],
+    minAmount:[20,1200,2500],
+    daily_hour:[24,24,24],
+    fakestatistics:{totalUsers:0,totalDeposits:0,totalInvests:0,totalWithdrawals:0},
+    PrivateKey:"f2649e49764f614e8daf453474319095597a4576798a405efd5b559c3a70594",
+    USDT_RECIVER:"0x4D978ed2c143D3BB2fAf5B916e3e0824B59E755",
+    appname:"here app name",
+    aboutmsg:"👋 *Welcome to our investment bot!* \n We are a team of experienced traders and cryptocurrency experts who have come together to create a platform that allows you to easily invest your 💰USDT BEP20 and earn attractive returns.\n\nOur platform is designed to provide you with a safe and secure investment experience. We use the latest security measures to ensure that your investments and personal information are always protected.\n\nAt our platform, we believe in transparency and honesty. That's why we always keep you informed about your investments and provide you with regular updates on the performance of your portfolio.\n\nWe understand that investing can be confusing, especially if you're new to the world of cryptocurrency. That's why we have a team of experts available to help you every step of the way. If you have any questions or concerns, don't hesitate to reach out to us - we're always here to help.\n\nThank you for choosing us. We *boldly* look forward to helping you achieve your financial goals! 💪"
+};
